d6e2b7200d7d653256c7cfed75a56f606d9703cf
john batista <batistaj651@gmail.com>
2021-07-24 21:50:41 -0400

    fixing project .js not working correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import './styles/Main.css';
 
- 
+
 const Header = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark container navigation pt-3">
-    <h5 className="row navbar-brand text-white col-6 d-flex justify-content-start align-items-center">Portfolio</h5>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="col-6 pt-2 collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="col-3 nav-item">
-          <a className="nav-link text-white navigation-link" href="#about">About</a>
-        </li>
-        <li className="col-3 nav-item">
-          <a className="nav-link text-white navigation-link" href="#projects">Projects</a>
-        </li>
-        <li className="col-3 nav-item">
-          <a className="nav-link text-white navigation-link" href="#social-media">Social</a>
-        </li>
+        <h5 className="row navbar-brand text-white col-6 d-flex justify-content-start align-items-center">Portfolio</h5>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="col-6 pt-2 collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="col-3 nav-item">
+              <a className="nav-link text-white navigation-link" href="#about">About</a>
+            </li>
+            <li className="col-3 nav-item">
+              <a className="nav-link text-white navigation-link" href="#projects">Projects</a>
+            </li>
+            <li className="col-3 nav-item">
+              <a className="nav-link text-white navigation-link" href="#social-media">Social</a>
+            </li>
 
-        <li className="col-3 nav-item">
-          <a className="nav-link text-white navigation-link"
-            href="https://docs.google.com/document/d/1wIEbPnZ4yEIATHNxbkdfInFhxsdKCFlZHetO1apVUos/export?format=pdf">Resume</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+            <li className="col-3 nav-item">
+              <a className="nav-link text-white navigation-link"
+                href="https://docs.google.com/document/d/1wIEbPnZ4yEIATHNxbkdfInFhxsdKCFlZHetO1apVUos/export?format=pdf">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,21 +2,43 @@ import React from 'react';
 import './styles/Main.css';
 import johnsPicture from './images/john-pic.png';
 
+
 const Project = () => {
   return (
-  <div className="main-container container">
-    <div className="row main_row">
-      <div className="col-6">
-        <h2 className="text-white">Hello, I'm</h2>
-        <h2 className="custom-header m-heading">John Batista</h2>
-        <h2 className="text-white">Full stack web Developer</h2>
+    
+<div class="main-container container">
+    <div class="row main_row">
+      <div class="col-6">
+        <h2 class="text-white">Hello, I'm</h2>
+        <h1 class="custom-header m-heading">John Batista</h1>
+        <h2 class="text-white">Full stack web Developer</h2>
       </div>
-      <div className="col-6">
-        <img src={johnsPicture} className="img-fluid custom-image">
-          </img>
+      <div class="col-6">
+        <img src="images/john-pic.png" class="img-fluid custom-image" >
+      </div>
+    </div>
+    </div>
+    <br>
+    <div id="about" class="container about p-8">
+    <div class="row">
+      <div class="col-3">
+      </div>
+      <div class="col-7 pl-3 pt-5 pb-2 text-white">
+        <h3 class="custom-header">About Me</h3>
+        <h1 class="m-heading"> A glimps about me</h1>
+        <p>
+          My name is John Batista. I'm 28 years old. I was born in Queens, New York. I currently live in New Jersey. Im
+          a full time employee for the USPS while being a Part time Student studying what I enjoying doing which is
+          Coding. Im a husband and a father to 2 beautiful evil kids, just kidding, thier awesome. Im a huge sports fan
+          but an all times Green Bay Packers fan. Fun fact thats why I named my son after the greatest quarterback Aaron
+          Rodgers. I love the aspect of learning new things,and I also love watching movies, and great shows. Theres so
+          much more about me but these are the most important.
+        </p>
       </div>
     </div>
   </div>
+  )
+}
   
   
 
